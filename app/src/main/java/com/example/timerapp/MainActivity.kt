89bcd53e0568e938a62188bc3f47a2cc6ff2924c
Akiko-Shinohara <akiko.shinohara@lireses.com>
2021-02-04 15:49:30 +0900

    数値no加算/TextViewの表示を更新

--- app/src/main/java/com/example/timerapp/MainActivity.kt
+++ app/src/main/java/com/example/timerapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.timerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    // 一度だけ代入するものはvalを使う //
+    val handler = Handler()
+    // 繰り返し代入するためvarを使う //
+    var timeValue = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // View要素を変数に代入 //
+        val timeText = findViewById(R.id.timeText) as TextView
+        val start = findViewById(R.id.start) as TextView
+        val stop = findViewById(R.id.stop) as TextView
+
+        // 1秒ごとに処理を実行
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                // TextViewを更新
+                // ?.letを用いて、nullではない場合のみ更新
+                timeToText()?.let {
+                    // timeToText(timeValue)の値がlet内ではitとして使える
+                    timeText.text = it as CharSequence?
+                }
+                handler.postDelayed(this, 1000)
+            }
+
+            private fun timeToText() {
+
+            }
+        }
+
+
     }
+
+
 }
