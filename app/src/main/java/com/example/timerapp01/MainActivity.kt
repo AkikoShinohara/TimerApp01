ea3e3bea348509eb4a59025f7213937bbe62b878
Akiko-Shinohara <akiko.shinohara@lireses.com>
2021-02-05 15:12:33 +0900

    コメントアウと追加

--- app/src/main/java/com/example/timerapp01/MainActivity.kt
+++ app/src/main/java/com/example/timerapp01/MainActivity.kt
@@ -8,24 +8,26 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    val handler = Handler()
-    var timeValue = 0
+    val handler = Handler() // 1度だけ移入するものはvalを使う
+    var timeValue = 0       // 繰り返し代入するためvarを使う
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // View(xml)要素を変数に代入
         val timeText = findViewById<TextView>(R.id.timeText)
         val startTap = findViewById<TextView>(R.id.tV)
         val stopTap = findViewById<TextView>(R.id.tV2)
 
-
+        //1秒ごとに処理を実行
         val runnable = object : Runnable {
             override fun run() {
                 timeValue++
-
+                // textView を更新
+                // ?.letを用いて、nullではない場合のみ更新
                 timeToText(timeValue)?.let {
+                    // timeToText(timeValue)の値がlet内ではitとして使える
                     timeText.text = it
                 }
                 handler.postDelayed(this, 1000)
@@ -33,17 +35,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         startTap.setOnClickListener {
-            handler.post(runnable)                // 最初のキュー登録
+            handler.post(runnable)                // start クリック処理されると 23秒から実行される
         }
         // stopボタン押された時の処理
         stopTap.setOnClickListener {
-            handler.removeCallbacks(runnable)      // キューキャンセル
+            handler.removeCallbacks(runnable)      // stop クリック処理
         }
 
     }
 
-
+    // 数値を00:00:00形式の文字列に変換する関数
+    // 引数timeにはデフォルト値0を設定、返却する型はnullableなString?型
     fun timeToText(time: Int = 0): String? {
+        // if式は値を返すため、そのままreturnできる
         return if (time < 0) {
             null
         } else if (time == 0) {
